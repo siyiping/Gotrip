344fd22ba33aef664733056960d14d99d2b46790
siyiping <siyiping33@163.com>
2015-06-29 18:52:37 +0800

    公司做的少来代码

--- app/src/main/java/com/siyiping/gotrip/ui/ConfirpasswordActivity.java
+++ app/src/main/java/com/siyiping/gotrip/ui/ConfirpasswordActivity.java
@@ -2,23 +2,34 @@ package com.siyiping.gotrip.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.siyiping.gotrip.R;
 
 public class ConfirpasswordActivity extends AppCompatActivity {
 
+    private TextView mPassword;
+    private TextView mPasswordconfirn;
+    private TextView mNickname;
+    private Button mButtoncomplete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirpassword);
+
+        initView();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.confirnpassword, menu);
         return true;
     }
 
@@ -35,5 +46,79 @@ public class ConfirpasswordActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initView(){
+        mButtoncomplete=(Button)findViewById(R.id.finish_signup);
+        mButtoncomplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mPassword=(TextView)findViewById(R.id.input_password);
+        mPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPasswordconfirn=(TextView)findViewById(R.id.input_confirn_password);
+        mPasswordconfirn.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mNickname=(TextView)findViewById(R.id.nickname);
+        mNickname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private boolean ispasswordlegal(){
+        int length=mPassword.length();
+        if(length<6){
+            mPassword.setError(getString(R.string.password_too_short));
+            return false;
+        }else if(length>15){
+            mPassword.setError(getString(R.string.password_too_long));
+            return false;
+        }else{
+            return true;
+        }
     }
 }
