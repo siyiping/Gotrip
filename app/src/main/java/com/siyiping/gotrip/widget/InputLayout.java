e4cf90f09dd3d7c2885830583977d0a1d3787bac
siyiping <siyiping33@163.com>
2015-07-04 18:10:03 +0800

    自定义控件背景

--- app/src/main/java/com/siyiping/gotrip/widget/InputLayout.java
+++ app/src/main/java/com/siyiping/gotrip/widget/InputLayout.java
@@ -3,6 +3,7 @@ package com.siyiping.gotrip.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -15,6 +16,8 @@ import com.example.siyiping.gotrip.R;
  */
 public class InputLayout extends LinearLayout {
 
+    private String text;
+    private String texthint;
 
     public InputLayout(Context context){
         this(context,null);
@@ -28,15 +31,26 @@ public class InputLayout extends LinearLayout {
         super(context, attrs, defStyleAttr);
 
         TypedArray t=context.obtainStyledAttributes(attrs, R.styleable.InputLayout);
-
+        text=t.getString(R.styleable.InputLayout_text);
+        texthint=t.getString(R.styleable.InputLayout_texthint);
+        t.recycle();
 
         TextView guide=new TextView(context, attrs, defStyleAttr);
         EditText input=new EditText(context, attrs, defStyleAttr);
 
-        this.addView(guide, (int)getResources().getDimension(R.dimen.widget_textview_width), ViewGroup.LayoutParams.MATCH_PARENT);
-        input.setMaxLines(1);
+        guide.setText(text);
+        this.addView(guide, (int) getResources().getDimension(R.dimen.widget_textview_width), ViewGroup.LayoutParams.MATCH_PARENT);
 
+        input.setMaxLines(1);
+        input.setHint(texthint);
         this.addView(input, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        input.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+            }
+        });
 
     }
 
